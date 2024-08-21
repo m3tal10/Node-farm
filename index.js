d2a5c63b8c936083ebe4e6a624f3f5338c77875f
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2024-08-21 23:26:54 +0600

    Changed localhost to '/'

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ const server = http.createServer((req, res) => {
   }
 });
 const port = process.env.PORT;
-server.listen(port, '127.0.0.1', () => {
+server.listen(port, '/', () => {
   console.log(`Listening to request on port ${port}`);
 });
